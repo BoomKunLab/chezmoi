0ed485f70831b0733e8019e44335e9c8e58db879
BoomKunLab <boomkun@yoga14s2021itl.>
2024-12-23 23:46:08 +0900

    update git_util

--- dot_shellrc.d/010_git_util.zsh
+++ dot_shellrc.d/010_git_util.zsh
@@ -7,13 +7,14 @@ if [ -f ~/.git_util/_git ] && [ -f ~/.git_util/git-completion.bash ]; then
     fpath=(~/.zsh $fpath)
     zstyle ':completion:*:*:git:*' script ~/.git_util/git-completion.bash
     autoload -Uz compinit && compinit
+
+
+    GIT_PS1_SHOWDIRTYSTATE=true
+    GIT_PS1_SHOWUNTRACKEDFILES=true
+    GIT_PS1_SHOWSTASHSTATE=true
+    GIT_PS1_SHOWUPSTREAM=auto
+
+    setopt PROMPT_SUBST
+
+    PS1='%n: %F{cyan}%c%f %F{magenta}$(__git_ps1 "(%s)")%f\$ '
 fi
-
-GIT_PS1_SHOWDIRTYSTATE=true
-GIT_PS1_SHOWUNTRACKEDFILES=true
-GIT_PS1_SHOWSTASHSTATE=true
-GIT_PS1_SHOWUPSTREAM=auto
-
-setopt PROMPT_SUBST
-
-PS1='%n: %F{cyan}%c%f %F{magenta}$(__git_ps1 "(%s)")%f\$ '
