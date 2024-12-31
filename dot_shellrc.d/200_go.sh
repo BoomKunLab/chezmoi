38cbd9e7bcbcc5a5102ca0d96900aa2e400bcb01
BoomKunLab <boomkun@yoga14s2021itl.>
2024-12-31 16:18:13 +0900

    update powerline configuration.

--- dot_shellrc.d/200_go.sh
+++ dot_shellrc.d/200_go.sh
@@ -6,3 +6,12 @@ if [ -d ~/.asdf/installs/golang ]; then
     #     export PATH=$PATH:~/.asdf/installs/golang/$version/go/bin
     # done
 fi
+
+# installed by mise
+if [ -d ~/.local/share/mise/installs/go ]; then
+    export GOPATH=~/.go
+    # export PATH=$PATH:$GOPATH/bin
+    # for version in $(ls ~/.local/share/mise/installs/go); do
+    #     export PATH=$PATH:~/.local/share/mise/installs/go/$version/bin
+    # done
+fi
