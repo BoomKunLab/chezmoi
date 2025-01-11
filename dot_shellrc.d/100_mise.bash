50eb53112fca42c44bbb6f29dcbc569c604c410b
BoomKunLab <boomkun@UX3405MA.>
2025-01-12 06:38:03 +0900

    update tool-completions

--- dot_shellrc.d/100_mise.bash
+++ dot_shellrc.d/100_mise.bash
@@ -1,4 +1,5 @@
 # mise installed by brew on bash
 if [ -f $HOMEBREW_PREFIX/bin/mise ]; then
     eval "$($HOMEBREW_PREFIX/bin/mise activate bash)"
+    source <($HOMEBREW_PREFIX/bin/mise completion bash)
 fi
--- dot_shellrc.d/100_mise.zsh
+++ dot_shellrc.d/100_mise.zsh
@@ -1,4 +1,5 @@
 # mise installed by brew on zsh
 if [ -f $HOMEBREW_PREFIX/bin/mise ]; then
     eval "$($HOMEBREW_PREFIX/bin/mise activate zsh)"
+    source <($HOMEBREW_PREFIX/bin/mise completion zsh)
 fi
