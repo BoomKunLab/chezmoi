662890169e4dfb776852cac76ec3460f7cfe1a62
BoomKunLab <80441830+BoomKunLab@users.noreply.github.com>
2025-11-19 18:00:07 +0900

    UPDATE: 不要 alias の削除

--- dot_shellrc.d/010_aliases.sh
+++ dot_shellrc.d/010_aliases.sh
@@ -17,9 +17,3 @@ fi
 alias ll='ls -l'
 alias la='ls -lA'
 alias l='ls -CF'
-alias code='cursor'
-
-# Add an "alert" alias for long running commands.  Use like so:
-# sleep 10; alert
-# alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
-
