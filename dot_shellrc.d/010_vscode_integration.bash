c92539b3a8eeec1320cbeb1744b4c1b7b0c2c59e
BoomKunLab <80441830+BoomKunLab@users.noreply.github.com>
2025-11-19 18:00:56 +0900

    UPDATE: vscode 系エディタの統合を cursor,kiro,agy に対応

--- dot_shellrc.d/010_vscode_integration.bash
+++ dot_shellrc.d/010_vscode_integration.bash
@@ -1,2 +1,11 @@
 # vscode bash
-[[ "$TERM_PROGRAM" == "vscode" ]] && . "$(code --locate-shell-integration-path bash)"
+if [[ "$TERM_PROGRAM" == "vscode" ]]; then
+  for cmd in code cursor kiro antigravity; do
+    if command -v "$cmd" >/dev/null 2>&1; then
+      if [ -f "$($cmd --locate-shell-integration-path bash)" ]; then
+        . "$($cmd --locate-shell-integration-path bash)"
+        break
+      fi
+    fi
+  done
+fi
--- dot_shellrc.d/010_vscode_integration.zsh
+++ dot_shellrc.d/010_vscode_integration.zsh
@@ -1,2 +1,11 @@
 # vscode zsh
-[[ "$TERM_PROGRAM" == "vscode" ]] && . "$(code --locate-shell-integration-path zsh)"
+if [[ "$TERM_PROGRAM" == "vscode" ]]; then
+  for cmd in code cursor kiro antigravity; do
+    if command -v "$cmd" >/dev/null 2>&1; then
+      if [ -f "$($cmd --locate-shell-integration-path zsh)" ]; then
+        . "$($cmd --locate-shell-integration-path zsh)"
+        break
+      fi
+    fi
+  done
+fi
